93f56a590a1e8a76fd83e1788748cb2fa07b14de
Jasmine Ng <jasminee.ngx@gmail.com>
2020-05-24 16:19:45 +0200

    add missing file

---
+++ src/constant/variables.ts
@@ -0,0 +1,16 @@
+export enum Command {
+  TEXT_BOLD = "bold",
+  TEXT_ITALIC = "italic",
+  TEXT_UNDERLINE = "underline",
+  FONT_COLOR = "forecolor",
+  FONT_FAMILY = "fontName",
+  FONT_SIZE = "fontSize",
+  ALIGN_LEFT = "justifyLeft",
+  ALIGN_RIGHT = "justifyRight",
+  ALIGN_CENTER = "justifyCenter",
+}
+
+export enum BackGroundType {
+  IMAGE = "image",
+  COLOR = "color",
+}
---
+++ src/text_editor/text_editor_component.interfaces.ts
@@ -0,0 +1,6 @@
+export interface TextEditorComponentProps {
+  initTextHTML: string;
+  textColor?: string;
+  bgColor?: string;
+  bgImage?: string;
+}
